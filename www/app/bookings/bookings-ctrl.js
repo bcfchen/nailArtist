2427d1883cc86234a76bb2c80ce0a566678a43f8
Beck Chen <bcfchen@gmail.com>
2015-11-21 22:57:26 -0800

    add bookings screen

--- www/app/bookings/bookings-ctrl.js
+++ www/app/bookings/bookings-ctrl.js
@@ -1,9 +1,26 @@
 (function(){
 	'use strict';
-	angular.module('nailArtist').controller('BookingsCtrl', ["$stateParams", "$firebaseArray", BookingsCtrl]);
+	angular.module('nailArtist').controller('BookingsCtrl', ["$stateParams", "$firebaseArray", "constants",BookingsCtrl]);
 
-	function BookingsCtrl($stateParams, $firebaseArray){
+	function BookingsCtrl($stateParams, $firebaseArray, constants){
 		var vm = this;
 		var productId = $stateParams.productId;
+		vm.selectedDate = {};
+		vm.selectedTime = {};
+
+		var ref = new Firebase(constants.FIREBASE_URL + "/schedule");
+		vm.schedule = $firebaseArray(ref);
+		vm.schedule.$loaded(function(date){
+			vm.selectDate(date[0]);
+		});
+
+		vm.selectDate = function(date){
+			vm.selectedDate = date.$id;
+			vm.times = Object.keys(date.times);
+		}
+
+		vm.selectTime = function(time){
+
+		}
 	};
 })();
