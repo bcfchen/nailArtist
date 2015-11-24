aeca481117c4ea890b58780bea6788b0a9e822a3
Beck Chen <bcfchen@gmail.com>
2015-11-23 20:56:42 -0800

    Style selector buttons for bookings page

--- www/app/bookings/bookings-ctrl.js
+++ www/app/bookings/bookings-ctrl.js
@@ -6,23 +6,40 @@
 		var vm = this;
 		vm.selectedDate = {};
 		vm.selectedTime = {};
+		vm.selectedAddress = {};
+
+		/* dummy address */
+		vm.addresses = [{
+			type: "Home",
+			text: "One Harrison St"
+		}, {
+			type: "Work",
+			text: "One Market St"
+		}, {
+			type: "Custom",
+			text: "86 Malta Dr"
+		}];
 
 		initialize();
 		var ref = new Firebase(constants.FIREBASE_URL + "/schedule");
 		vm.schedule = $firebaseArray(ref);
 		vm.schedule.$loaded(function(dates){
 			attachDateProperties(dates);
-			vm.selectDate(dates[0]);
+			//vm.selectDate(dates[0]);
 		});
 
 		vm.selectDate = function(date){
 			vm.selectedDate = date.$id;
 			vm.times = Object.keys(date.times);
-			vm.selectTime(vm.times[0]);
+			//vm.selectTime(vm.times[0]);
 		}
 
 		vm.selectTime = function(time){
 			vm.selectedTime = time;
+		}
+
+		vm.selectAddress = function(address){
+			vm.selectedAddress = address;
 		}
 
 		function initialize(){
