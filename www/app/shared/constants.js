6a0fc5be7934a2042495e83c054cdda56da659cb
Beck Chen <bcfchen@gmail.com>
2015-12-07 20:09:57 -0800

    update bookings page logic

--- www/app/shared/constants.js
+++ www/app/shared/constants.js
@@ -2,6 +2,8 @@
     'use strict';
     angular.module('nailArtist').value('constants', {
     	FIREBASE_URL: "https://nailartist.firebaseio.com",
-    	SERVER_URL: "https://morning-island-6861.herokuapp.com/charge"
+    	    	SERVER_URL: "http://localhost:3006/charge"
+
+    	//SERVER_URL: "https://morning-island-6861.herokuapp.com/charge"
     });
 })();
